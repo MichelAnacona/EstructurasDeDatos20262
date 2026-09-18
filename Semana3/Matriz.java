14cf20dc02532470bd106082547ab27aa5de914f
MichelAnacona <michel.anacona@campusucc.edu.co>
2026-09-18 23:58:44 +0000

    Clase de matrices con metodos

---
+++ Semana3/Matriz.java
@@ -0,0 +1,20 @@
+public class OperacionesMAtrices {
+    public int[][] llenarMAtriz(int f, int c, int min, int max) {
+        int[][] m = new int[f][c];
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++) {
+                m[i][j] = (int) (Math.random() * (max - min - 1) + min);
+            }
+        }
+        return m;
+    }
+    public String imprimirMatriz(int [][] m) {
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++) {
+                m[i][j] = (int) (Math.random() * (max - min - 1) + min);
+            }
+        }
+        return "";
+    }
+
+}
