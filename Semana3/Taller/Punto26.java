02058d64eb4c9605f0b9021adb4ac4aaad23599e
MichelAnacona <michel.anacona@campusucc.edu.co>
2026-09-24 20:51:42 +0000

    update: delete comentarios

--- Semana3/Taller/Punto26.java
+++ Semana3/Taller/Punto26.java
@@ -11,12 +11,10 @@ public class Punto26 {
         String[] distribuidoras = {"Norma", "Caribe", "Alpes", "Norma", "Caribe", "Alpes", "Norma", "Caribe"};
         int[] precios = {3500, 4200, 2800, 5000, 3900, 4500, 3000, 6000};
 
-        // filas: modelos (8), columnas: dias (30) -> cantidad de cuadernos vendidos
         int[][] ventas = objM.llenarMAtriz(8, 30, 0, 20);
-        // dinero recaudado = cantidad vendida * precio del modelo
+  
         int[][] recaudo = objM.recaudoPorModelo(ventas, precios);
 
-        // a. total recaudado por modelo
         int[] totalModelo = objM.totalesPorFila(recaudo);
         System.out.println("a. Total recaudado por modelo en los 30 dias:");
         for (int i = 0; i < totalModelo.length; i++) {
@@ -24,14 +22,12 @@ public class Punto26 {
                     + "): $" + totalModelo[i]);
         }
 
-        // b. total recaudado por dia
         int[] totalDia = objM.totalesPorColumna(recaudo);
         System.out.println("\nb. Total recaudado por dia:");
         for (int j = 0; j < totalDia.length; j++) {
             System.out.println("   Dia " + (j + 1) + ": $" + totalDia[j]);
         }
 
-        // c. modelo que mas dinero produjo
         int modeloMayor = objM.posicionMayor(totalModelo);
         System.out.println("\nc. Modelo que mas dinero produjo: " + modelos[modeloMayor]
                 + " (" + distribuidoras[modeloMayor] + ") con $" + totalModelo[modeloMayor]);
